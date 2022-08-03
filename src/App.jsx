ac09d4cbaf2879a2245e0656c26f067f55f005fe
Will Pedroso <willyng.pedroso@gmail.com>
2022-08-03 05:13:54 -0300

    add contributors

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ export const App = () => {
                 <p><strong>v1.0</strong></p>
                 <p>Develop by <a href="https://github.com/willypedroso" rel="noopener noreferrer" target="_blank">Will Pedroso</a></p>
                 <p>Based on the original repository by <a href="https://dev.to/danielle8farias/series/5484" rel="noopener noreferrer" target="_blank">danielle8farias</a></p>
+                <p>Contributors: <a href="https://github.com/RickelmeDias" rel="noopener noreferrer" target="_blank">RickelmeDias</a></p>
             </footer>
 </div>
     )
